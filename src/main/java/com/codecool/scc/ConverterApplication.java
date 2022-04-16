9867a195ebf3150f35447f9e3fa0b8786b19b509
Vincze Gábor <vinczegabor1981@gmail.com>
2022-04-16 17:40:14 +0200

    add message if no program arguments were given then program exits

--- src/main/java/com/codecool/scc/ConverterApplication.java
+++ src/main/java/com/codecool/scc/ConverterApplication.java
@@ -2,6 +2,11 @@ package com.codecool.scc;
 
 public class ConverterApplication {
     public static void main(String[] args) {
+        if(args.length == 0)
+        {
+            System.out.println("No input file defined!");
+            return;
+        }
         
     }
 }

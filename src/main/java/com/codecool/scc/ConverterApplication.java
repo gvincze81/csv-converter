e07549c604e685b38395b8e6b83a17f6f375d6f4
Vincze Gábor <vinczegabor1981@gmail.com>
2022-04-16 19:44:19 +0200

    handle command-line arguments

--- src/main/java/com/codecool/scc/ConverterApplication.java
+++ src/main/java/com/codecool/scc/ConverterApplication.java
@@ -1,12 +1,30 @@
 package com.codecool.scc;
 
+import java.nio.file.Path;
+import java.util.Optional;
+
 public class ConverterApplication {
     public static void main(String[] args) {
+        Path path;
+        Optional<String> format;
         if(args.length == 0)
         {
             System.out.println("No input file defined!");
             return;
         }
-        
+
+        if(args.length == 1)
+        {
+            path = Path.of(args[0]);
+            System.out.println(path);
+            return;
+        }
+
+        path = Path.of(args[1]);
+        format = Optional.of(args[0]);
+
+        System.out.println(path);
+        System.out.println(format.get());
+
     }
 }
